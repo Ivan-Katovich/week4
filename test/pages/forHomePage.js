449ba1bf3ce8c24fba081d7b11f5f999e43e8300
SVM-1748 <you@example.com>
2018-07-12 00:26:45 +0300

    xpath update

--- test/pages/forHomePage.js
+++ test/pages/forHomePage.js
@@ -10,14 +10,14 @@ class ForHomePage extends Page {
         this.url = 'https://www.sandisk.com/home';
 
         this.data = {
-            mobileStorage: element(by.css('nav.navbar:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)')),
-            cardsReaders: element(by.xpath('/html/body/div[2]/div/header/div[2]/div/div[3]/nav/div[2]/ul/li[2]/a')),
-            usbFlash: element(by.xpath('/html/body/div[2]/div/header/div[2]/div/div[3]/nav/div[2]/ul/li[3]/a')),
-            ssd: element(by.xpath('/html/body/div[2]/div/header/div[2]/div/div[3]/nav/div[2]/ul/li[4]/a')),
-            mp3Players: element(by.xpath('/html/body/div[2]/div/header/div[2]/div/div[3]/nav/div[2]/ul/li[5]/a')),
-            extremeTeam: element(by.xpath('/html/body/div[2]/div/header/div[2]/div/div[3]/nav/div[2]/ul/li[6]/a')),
-            stories: element(by.xpath('/html/body/div[2]/div/header/div[2]/div/div[3]/nav/div[2]/ul/li[7]/a')),
-            shopNow: element(by.xpath('/html/body/div[2]/div/header/div[2]/div/div[3]/nav/div[2]/ul/li[8]/a'))
+            mobileStorage: element(by.xpath('//*[@id="navbar"]/div[3]/nav/div[2]/ul/li[1]/a')),
+            cardsReaders: element(by.xpath('//*[@id="navbar"]/div[3]/nav/div[2]/ul/li[2]/a')),
+            usbFlash: element(by.xpath('//*[@id="navbar"]/div[3]/nav/div[2]/ul/li[3]/a')),
+            ssd: element(by.xpath('//*[@id="navbar"]/div[3]/nav/div[2]/ul/li[4]/a')),
+            mp3Players: element(by.xpath('//*[@id="navbar"]/div[3]/nav/div[2]/ul/li[5]/a')),
+            extremeTeam: element(by.xpath('//*[@id="navbar"]/div[3]/nav/div[2]/ul/li[6]/a')),
+            stories: element(by.xpath('//*[@id="navbar"]/div[3]/nav/div[2]/ul/li[7]/a')),
+            shopNow: element(by.xpath('//*[@id="navbar"]/div[3]/nav/div[2]/ul/li[8]/a'))
         };
 
         this.mobileStorageTitle = 'Mobile Storage | SanDisk';
