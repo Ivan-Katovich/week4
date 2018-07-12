78d1a7c3901a62ec91f39985b8d41762c9b501aa
sergey.martinovich <sergey.martinovich@itechart-group.com>
2018-07-12 16:02:32 +0300

    added filter

--- test/pages/mainPage.js
+++ test/pages/mainPage.js
@@ -17,10 +17,23 @@ class MainPage extends Page {
             support: element(by.xpath('//*[@id="bs-example-navbar-collapse-1"]/ul/li[5]/a'))
         };
 
+        this.menuElements = element.all(by.css('.nav-wrapper li'));
+
         this.forHomeTitle = 'Global Leader in Flash Memory Storage Solutions | SanDisk';
         this.forBusinessTitle = 'For Business - Sandisk Flash Storage Solutions';
         this.oemDesignTitle = 'Sandisk - OEM Design Solutions';
     }
+
+    clickMenuElement(item) {
+         const rootEl = this.menuElements.filter((el) => {
+            return el.getText()
+                .then((text) => {
+                    return text === item;
+                });
+        }).first();
+        return rootEl.click();
+    }
+
 }
 
 module.exports = MainPage;
--- test/spec/spec.js
+++ test/spec/spec.js
@@ -13,33 +13,33 @@ describe('Opening pages and displaying products', function () {
     });
 
     it('1 - should open For Home page', function () {
-        return mainPage.clickElement('forHome')
+        return mainPage.clickMenuElement('For Home')
             .then ( () => browser.getTitle())
             .then ( text => expect(text).toEqual(mainPage.forHomeTitle))
         }
     );
 
-    it('2 - should open For Business page', function () {
-        return mainPage.clickElement('forBusiness')
-                .then ( () => browser.getTitle())
-                .then ( text => expect(text).toEqual(mainPage.forBusinessTitle))
-        }
-    );
-
-    it('3 - should open OEM Design page', function () {
-        return mainPage.clickElement('oemDesign')
-                .then ( () => browser.getTitle())
-                .then ( text => expect(text).toEqual(mainPage.oemDesignTitle))
-        }
-    );
-
-    it('1.1 - should display Mobile Storage section on the For Home page', function () {
-        return mainPage.clickElement('forHome')
-                .then ( () => forHomePage.clickElement('mobileStorage'))
-                .then ( () => browser.getTitle())
-                .then ( text => expect(text).toEqual(forHomePage.mobileStorageTitle))
-        }
-    );
+    // it('2 - should open For Business page', function () {
+    //     return mainPage.clickElement('forBusiness')
+    //             .then ( () => browser.getTitle())
+    //             .then ( text => expect(text).toEqual(mainPage.forBusinessTitle))
+    //     }
+    // );
+    //
+    // it('3 - should open OEM Design page', function () {
+    //     return mainPage.clickElement('oemDesign')
+    //             .then ( () => browser.getTitle())
+    //             .then ( text => expect(text).toEqual(mainPage.oemDesignTitle))
+    //     }
+    // );
+    //
+    // it('1.1 - should display Mobile Storage section on the For Home page', function () {
+    //     return mainPage.clickElement('forHome')
+    //             .then ( () => forHomePage.clickElement('mobileStorage'))
+    //             .then ( () => browser.getTitle())
+    //             .then ( text => expect(text).toEqual(forHomePage.mobileStorageTitle))
+    //     }
+    //);
  });
 
 
